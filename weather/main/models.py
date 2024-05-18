26ab23a897cf1c18e10659442836dad37f1b1ba5
gulser <glserozkan@gmail.com>
2024-05-18 19:51:59 +0200

    city and weather models are added

--- weather/main/models.py
+++ weather/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}: {self.country}"
+
+
+class Weather(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    temperature = models.FloatField()
+    description = models.CharField(max_length=250)
+    icon = models.CharField(max_length=20)
+    date_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.city}- {self.date_time}"
+
+
--- weather/main/views.py
+++ weather/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import City, Weather
+from django.utils import timezone
 # When we ask weather, we need an function sends request to
 # WeatherAPI to get a weather response, response will be
 # in JSON format?, also urllib is needed?
@@ -9,14 +10,18 @@ import requests, json
 def main(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=90fdc1f0beb1ae5812273c9b26256614"
     #city = "Las Vegas"
-    city = "Kayseri"
-    city_weather = requests.get(url.format(city)).json()
+    response = requests.get(url.format(city)).json()
+    try:
+        response.raise_for_status()
+    except HttpError:
+        return 1
+        #print("Error: ", err)
     weather = {
         "city" : city,
-        "temperature" : city_weather["main"]["temp"],
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon']
+        "temperature" : response["main"]["temp"],
+        'description' : response['weather'][0]['description'],
+        'icon' : response['weather'][0]['icon']
     }
     context = {"weather" : weather}
-    print(json.dumps(city_weather, indent=4))
+    print(json.dumps(response, indent=4))
     return render(request, 'main/main.html', context)
