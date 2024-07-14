16e32f49734d32d00f24cf8065649d78a7e75d9c
gulser <glserozkan@gmail.com>
2024-07-14 11:17:12 +0200

    bound data db existence check doesnt work

--- weather/main/views.py
+++ weather/main/views.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 from .forms import CityForm
 from django.views.generic import ListView
 import requests, json
+#add messages to your views that can be displayed to users
+from django.contrib import messages
+
 
 # To use API_KEY
 from django.conf import settings
@@ -26,6 +29,7 @@ def search(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
 
     weather_list = []
+    #unbound form, no data to bound
     form = CityForm()
     context = {"weather_list" : weather_list, 'form' : form}
 
@@ -35,21 +39,31 @@ def search(request):
          # csrf token is automatically checked by the middleware
         
         #Create a form instance from POST data.
+        #bound data, not validated yet
+        #cleaned_data() get the content(after validation)
         form = CityForm(request.POST)
+        #validated form data will be in the form.cleaned_data dictionary
         if form.is_valid():
-            #city = form.save(commit=False)
             city = form.save()
-            response = requests.get(url.format(city.name, api_key)).json()
-            if response.get('cod') == 200:
-                weather = {
-                    "city" : city.name,
-                    "temperature" : response['main']['temp'],
-                    "description" : response['weather'][0]['description'],
-                    #"icon" : response['weather'][0]['icon']
-                    }
-                weather_list.append(weather)
+            
+            #get cityname from validated bound data
+            name = form.cleaned_data["name"]
+            print(name)
+            #check if data already exist in db
+            if City.objects.filter(name=name).exists():
+                messages.error(request, f"The city {name} already exists in the database.")
+            else:
+                response = requests.get(url.format(city.name, api_key)).json()
+                #import pdb;pdb.set_trace()
+                if response.get('cod') == 200:
+                    weather = {
+                        "city" : city.name,
+                        "temperature" : response['main']['temp'],
+                        "description" : response['weather'][0]['description'],
+                        #"icon" : response['weather'][0]['icon']
+                        }
+                    weather_list.append(weather)
             context = {"weather_list" : weather_list, 'form' : form}
-            #return redirect("main")
             #city.country = get_country(city.name)
             city.save()
 
@@ -59,28 +73,6 @@ def search(request):
     #     form = BlockForm()
 
     return render(request, 'main/main.html', context)
-    #form = BlockForm()
-
-    #print(citys)
-    # else:
-    #     blocks = Block.objects.all()
-    #     for block in blocks:
-    #         try:
-    #             #could be replaced w/ get_object_or_404()
-    #             response = requests.get(url.format(block.city, api_key)).json()
-    #             if response.get('cod') == 200:
-    #                 weather = {
-    #                     "city" : block.city,
-    #                     #"temperature" : response['main']['temp'],
-    #                     #"description" : response['weather'][0]['description'],
-    #                     #"icon" : response['weather'][0]['icon']
-    #                     }
-    #                 weather_list.append(weather)
-    #         except requests.exceptions.RequestException as e:
-    #             raise SystemExit(e)
-    #     context = {"weather_list" : weather_list, 'form' : form}
-    # #print(json.dumps(response, indent=4))
-    # return render(request, 'main/main.html', context)
 
 
 def get_country(city_name):
