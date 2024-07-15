065e7b64c282b940922c78584b883d012de44aa3
gulser <glserozkan@gmail.com>
2024-07-15 19:50:58 +0200

    double saving for empty obj is fixed

--- weather/main/views.py
+++ weather/main/views.py
@@ -62,7 +62,6 @@ def search(request):
         form = CityForm(request.POST)
         #validated form data will be in the form.cleaned_data dictionary
         if form.is_valid():
-            #city = form.save()
             #get cityname from validated bound data
             name = form.cleaned_data["name"]
             print(name)
@@ -70,8 +69,7 @@ def search(request):
             if City.objects.filter(name=name).exists():
                 messages.error(request, f"The city {name} already exists in the database.")
             else:
-                city = form.save()
-                response = requests.get(url.format(city.name, api_key)).json()
+                response = requests.get(url.format(name, api_key)).json()
                 #import pdb;pdb.set_trace()
                 if response.get('cod') == 200:
                     city = City(
