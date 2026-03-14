3640fd0d1abaaaeb6e9f26203c6f2aa3b44eb006
gulser <glserozkan@gmail.com>
2026-03-14 15:50:06 +0100

    ip address change in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,8 @@ python manage.py migrate
 # echo "migrating static files"
 # python manage.py collectstatic --noinput
 
+# echo "Starting Django server..."
+# exec python manage.py runserver 0.0.0.0:8000
+
 echo "Starting Django server..."
-exec python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver ${IP}:8000
